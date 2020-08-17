1432939d034167d4f1a1bff51d25d55bfd9dc050
Ryan Lee <therealsassyfatcat@outlook.com>
2020-08-17 18:53:19 -0500

    saving

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -5,6 +5,7 @@ export const fetchShow = () => {
       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
     )
     .then(res => {
+      console.log(res)
       return res
       // setShow(res.data);
       // setSeasons(formatSeasons(res.data._embedded.episodes));
---
+++ src/components/Episodes.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+test('nothing',
+() => {
+
+})
